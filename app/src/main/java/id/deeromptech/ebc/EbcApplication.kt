f03e7bc3b37fb64100bdfba39863c52d1f72c761
Tisna Hadiana <tisnahadiana@gmail.com>
2023-07-17 10:04:09 +0700

    Adding Splash Screen Success

---
+++ app/src/main/java/id/deeromptech/ebc/EbcApplication.kt
@@ -0,0 +1,7 @@
+package id.deeromptech.ebc
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EbcApplication : Application()
---
+++ app/src/main/java/id/deeromptech/ebc/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package id.deeromptech.ebc.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.deeromptech.ebc.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
