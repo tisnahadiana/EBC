1d6fe3bf5df1002600166883a36968415e2249aa
Tisna Hadiana <tisnahadiana@gmail.com>
2023-07-18 11:31:09 +0700

    Layout Register Finish

--- app/src/main/java/id/deeromptech/ebc/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/id/deeromptech/ebc/ui/auth/login/LoginActivity.kt
@@ -1,14 +1,26 @@
 package id.deeromptech.ebc.ui.auth.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
-import id.deeromptech.ebc.R
+import id.deeromptech.ebc.databinding.ActivityLoginBinding
+import id.deeromptech.ebc.ui.auth.register.RegisterActivity
+
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        binding.btnToRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/id/deeromptech/ebc/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/id/deeromptech/ebc/ui/auth/register/RegisterActivity.kt
@@ -1,12 +1,26 @@
 package id.deeromptech.ebc.ui.auth.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import id.deeromptech.ebc.R
+import dagger.hilt.android.AndroidEntryPoint
+import id.deeromptech.ebc.databinding.ActivityRegisterBinding
+import id.deeromptech.ebc.ui.auth.login.LoginActivity
 
+@AndroidEntryPoint
 class RegisterActivity : AppCompatActivity() {
+
+    private val binding: ActivityRegisterBinding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnToLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
